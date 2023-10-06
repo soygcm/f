08fa8d9995af624322432855f3566d4c5f1d10ba
Gabriel Castañaza <gabriel.castanaza@nextern.com>
2023-10-06 11:30:02 -0600

    re indent

--- perezoso/perezoso.js
+++ perezoso/perezoso.js
@@ -4,33 +4,33 @@
 function Pregunta(pregunta, maximoAceptado) {
     this.pregunta = pregunta;
     this.maximoAceptado = maximoAceptado;
-  }
-  
-  // Lista de preguntas
-  const preguntas = [
+}
+
+// Lista de preguntas
+const preguntas = [
     new Pregunta("¿Cuántas veces al día dejas para después algo que tenías que hacer?", 3),
     new Pregunta("¿Cuántas veces a la semana dejas ropa tirada en el cuarto hasta el día siguiente?", 1),
     new Pregunta("¿Cuántas veces a la semana no lavas los platos hasta el día siguiente?", 1),
     new Pregunta("¿Cuántas veces a la semana no barres?", 4)
-  ];
-  
-  // Función para averiguar si es perezoso
-  function averiguarSiEsPerezoso(pregunta) {
+];
+
+// Función para averiguar si es perezoso
+function averiguarSiEsPerezoso(pregunta) {
     const respuesta = Number(prompt(pregunta.pregunta));
     return respuesta > pregunta.maximoAceptado;
-  }
-  
-  // Programa principal
-  function main() {
+}
+
+// Programa principal
+function main() {
     const preguntasEsPerezoso = preguntas.map(pregunta => averiguarSiEsPerezoso(pregunta));
     const totalPerezosas = preguntasEsPerezoso.filter(esPerezoso => esPerezoso).length;
-  
+    
     if (totalPerezosas > 2) {
-      console.log("Eres una persona perezosa");
+        console.log("Eres una persona perezosa");
     } else {
-      console.log("No eres una persona perezosa");
+        console.log("No eres una persona perezosa");
     }
-  }
-  
-  // Ejecutar el programa principal
-  main();
+}
+
+// Ejecutar el programa principal
+main();
