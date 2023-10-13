c2e63bca749453f7bb0f61d24e5e220ea428a867
Gabriel Castañaza <gabriel.castanaza@nextern.com>
2023-10-12 21:17:54 -0600

    Cool

--- languages/haskell/app/Main.hs
+++ languages/haskell/app/Main.hs
@@ -2,27 +2,21 @@ module Main where
 
 import Test.HUnit
 
-packDiff rest start superList = pack rest ((start : []) : superList)
+pack :: Eq a => [a] -> [[a]]
+pack lst = packAux lst []
+  where
+    packAux [] acc = reverse acc
+    packAux (start:rest) [] = packAux rest [[start]]
+    packAux (start:rest) (superHead:superTail) =
+      if start == head superHead
+        then packAux rest ((start : superHead) : superTail)
+        else packAux rest ([start] : superHead : superTail)
 
-pack :: Eq a => [a] -> [[a]] -> [[a]]
-pack [] superList = superList
-pack (start:rest) [] = packDiff rest start []
-pack (start:rest) (superHead: superTail) = 
-    if start == head superHead then
-        pack rest ((start : superHead) : superTail)
-    else packDiff rest start (superHead: superTail)
-            
-    
 
 test_all = runTestTT $ TestList
   [ 
-     "Test 1" ~: pack [] [] ~?= ([] :: [[Char]])
-    ,"Test 2" ~: pack ["a"] [] ~?= [["a"]]
-    ,"Test 3" ~: pack ["a"] [["a"]] ~?= [["a", "a"]]
-    ,"Test 4" ~: pack ["a"] [["b"]] ~?= [["a"], ["b"]]
-  , "Test 7" ~: reverse (
-    pack ["a", "a", "a", "a", "b", "c", "c", "a", "a"] []
-    ) ~?= [["a", "a", "a", "a"], ["b"], ["c", "c"], ["a", "a"]]
+    "Test 1" ~: pack ["a", "a", "a", "a", "b", "c", "c", "a", "a"]
+    ~?= [["a", "a", "a", "a"], ["b"], ["c", "c"], ["a", "a"]]
   ]
 
 main = do
