cd0cebc3e5eeb725051f4cc1a30982361095b38d
Gabriel Castañaza <gabriel.castanaza@nextern.com>
2023-10-12 21:14:09 -0600

    Now it works

--- languages/haskell/app/Main.hs
+++ languages/haskell/app/Main.hs
@@ -2,23 +2,26 @@ module Main where
 
 import Test.HUnit
 
-pack :: Eq a => [[a]] -> [a] -> [[a]]
+packDiff rest start superList = pack rest ((start : []) : superList)
+
+pack :: Eq a => [a] -> [[a]] -> [[a]]
 pack [] superList = superList
-pack (x:xs) [] = pack xs [x]
-pack (x:xs) (y:ys) =
-  if head x == y
-    then pack xs ((x ++ y) : ys)
-    else pack xs (x : (y:ys))
+pack (start:rest) [] = packDiff rest start []
+pack (start:rest) (superHead: superTail) = 
+    if start == head superHead then
+        pack rest ((start : superHead) : superTail)
+    else packDiff rest start (superHead: superTail)
+            
+    
 
 test_all = runTestTT $ TestList
-  [ "Test 1" ~: pack [] [] ~?= ([] :: [[Int]])
-  , "Test 2" ~: pack [["a"]] [] ~?= [["a"]]
-  , "Test 3" ~: pack [["a"]] [["a"]] ~?= [["a", "a"]]
-  , "Test 4" ~: pack [["a"]] [["b"]] ~?= [["a"], ["b"]]
-  , "Test 5" ~: pack [["b"], ["a"]] [] ~?= [["a"], ["b"]]
-  , "Test 6" ~: pack [["a", "b"]] [["a"]] ~?= [["b"], ["a", "a"]]
+  [ 
+     "Test 1" ~: pack [] [] ~?= ([] :: [[Char]])
+    ,"Test 2" ~: pack ["a"] [] ~?= [["a"]]
+    ,"Test 3" ~: pack ["a"] [["a"]] ~?= [["a", "a"]]
+    ,"Test 4" ~: pack ["a"] [["b"]] ~?= [["a"], ["b"]]
   , "Test 7" ~: reverse (
-    pack [["a", "a", "a", "a", "b", "c", "c", "a", "a"]] []
+    pack ["a", "a", "a", "a", "b", "c", "c", "a", "a"] []
     ) ~?= [["a", "a", "a", "a"], ["b"], ["c", "c"], ["a", "a"]]
   ]
 
